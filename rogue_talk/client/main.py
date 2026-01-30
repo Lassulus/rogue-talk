2670b5f18cec1cce32073f793a03c8e7d23ec11a
pinpox <git@pablo.tools>
2026-01-30 02:42:46 +0100

    Make --name default to $USER

--- rogue_talk/client/main.py
+++ rogue_talk/client/main.py
@@ -2,6 +2,7 @@
 
 import argparse
 import asyncio
+import os
 
 from ..common.constants import DEFAULT_HOST, DEFAULT_PORT
 from .game_client import GameClient
@@ -11,7 +12,7 @@ def main() -> None:
     parser = argparse.ArgumentParser(description="Rogue-Talk Client")
     parser.add_argument("--host", default=DEFAULT_HOST, help="Server host")
     parser.add_argument("--port", type=int, default=DEFAULT_PORT, help="Server port")
-    parser.add_argument("--name", required=True, help="Player name")
+    parser.add_argument("--name", default=os.environ.get("USER", "player"), help="Player name")
     args = parser.parse_args()
 
     client = GameClient(args.host, args.port, args.name)
